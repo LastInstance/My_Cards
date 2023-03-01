2a732ef8d11237b7e780d926f881d4526cdc6129
Artem <vulpesinculta22@gmail.com>
2023-03-01 19:02:05 +0200

    Modifyed cards.py

---
+++ try.py
@@ -0,0 +1,109 @@
+import random, itertools
+
+class CardDeck():
+    ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
+    small_ranks = [6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
+    suits = ['hearts', 'spades', 'diamonds', 'clubs']
+    shuffle_deck = False
+    deck = []
+
+    def shuffle(self):
+        random.shuffle(self.deck)
+        shuffle_deck = True
+        return self.deck
+
+    def __len__(self):
+        return len(self.deck)
+
+    def __getitem__(self, key):
+        item = self.deck[key]
+        if not isinstance(key, slice):
+            return (item, )
+        return tuple(item)
+
+    def __add__(self, new_card):
+        if isinstance(new_card, list):
+            for card in new_card:
+                self.deck.append(card)
+        else:
+            self.deck.append(new_card)
+        return self.deck
+
+    def __contains__(self, card):
+        return (card in self.deck)
+
+    def __eq__(self, other):
+        if self.deck == other:
+            return True
+        return False
+
+    def __sub__(self, cards_delete):
+        if isinstance(cards_delete, list):
+            for card in cards_delete:
+                self.deck.remove(card)
+        else:
+            self.deck.remove(cards_delete)
+        return self.deck
+
+
+class SmallDeck(CardDeck):
+    
+    def __init__(self):
+        self.deck = []
+        self.deck_cr()
+
+    def __str__(self):
+        return f'Deck is {self.deck}'
+
+    def deck_cr(self):
+        self.deck = list(itertools.product(CardDeck.small_ranks, CardDeck.suits))
+        return self.deck
+
+class ClassicDeck(CardDeck):
+    
+    def __init__(self):
+        self.deck = []
+        self.deck_cr()
+
+    def __str__(self):
+        return f'Deck is {self.deck}'
+
+    def deck_cr(self):
+        self.deck = list(itertools.product(CardDeck.ranks, CardDeck.suits))
+        return self.deck
+
+if __name__ == '__main__':
+    cards = [('Q', 'spades'), (10, 'hearts'), (7, 'diamonds')]
+    card = [('Q', 'spades')]
+    deck_1 = CardDeck()
+    deck_2 = SmallDeck()
+    deck_3 = ClassicDeck()
+    '''For check decks'''
+    # print(deck_2)
+    # print(deck_3)
+    '''For check shuffled decks'''
+    # deck_2.shuffle()
+    # deck_3.shuffle()
+    # print(deck_2)
+    # print(deck_3)
+    '''For check __len__'''
+    # print(len(deck_2))
+    # print(len(deck_3))
+    '''For check __add__'''
+    # deck_2 = deck_2 + cards
+    # print(len(deck_2))
+    # deck_3 = deck_3 + cards
+    # print(len(deck_3))
+    '''for check __sub__'''
+    # deck_2 = deck_2 - cards
+    # print(len(deck_2))
+    # deck_3 = deck_3 - cards
+    # print(len(deck_3))
+    '''for check __contains__'''
+    # print(('Q', 'spades') in deck_2)
+    # print(('Q', 'spades') in deck_3)
+    '''for check __getitem__'''
+    # print(deck_2[6])
+    # print(deck_3[3:9])
+
+
